75c07bd5e48030f39109f21b1e0e1481578b5387
Uladzislau Kandratovich <mrvini34@gmail.com>
2021-11-17 22:28:08 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () =>{
+    return(
+        <header className='header'>
+            <img src="https://lh3.googleusercontent.com/proxy/fVjzn83mpH_lNSlQDsWirRRLVcKltW4RhEVZTTUWmYuppgaL8SPloP8sLyoIQKb7EMFZOJ9Czg8kgmh601aeI5SihjuGpkp9RM7DAnpMp1K_Ag" alt="" className="header__logo" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+    return (
+        <nav className='sidebar__menu'>
+        <ul className='menu__inner'>
+          <li className='menu__item'><a href="#s" className='menu__link'>Profile</a></li>
+          <li className='menu__item'><a href="#s" className='menu__link'>Messages</a></li>
+          <li className='menu__item'><a href="#s" className='menu__link'>News</a></li>
+          <li className='menu__item'><a href="#s" className='menu__link'>Music</a></li>
+          <li className='menu__item'><a href="#s" className='menu__link'>Settings</a></li>
+        </ul>
+      </nav>
+    )
+}
+
+export default Navbar
